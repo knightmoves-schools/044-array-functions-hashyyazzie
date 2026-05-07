cb60571242db58288a186e38358de402ef4b778c
Craig <craigyazzie143@icloud.com>
2026-05-07 14:08:06 -0600

    Update index.js
    gg

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 let automobiles = ["car", "truck", "motorcycle"];
-// add to end
 automobiles.push("airplane", "skateboard");
-// add to FRONT
 automobiles.unshift("helicopter", "bike");
-// copy using temmplate string
 let automobilesCopy = `${automobiles}`;
-// remove LAST item
 automobiles.pop();
-// remove FIRST item
 automobiles.shift();
-// display result
 document.getElementById('result').innerHTML = automobiles;
